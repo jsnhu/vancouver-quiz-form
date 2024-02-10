053e7f1e9fb973c8d0f3e7a79e5e7edb499ac433
Jason Hu <jsnhu.os@gmail.com>
2024-02-09 19:03:23 -0800

    check all questions answered and get answers

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("working")
+quizAnswerKey = {
+    'question-1': 'c',
+    'question-2': 'd',
+    'question-3': 'a',
+    'question-4': 'a',
+    'question-5': 'd',
+}
+
+console.log("working");
+
+function checkQuiz() {
+    userSelections = document.querySelectorAll(`#quiz-form input[type='radio']:checked`);
+
+    if (!checkAllQuestionsAnswered(userSelections)) {
+        console.log('have not answered all questions');
+        return
+    }
+
+    getUserAnswers(userSelections);
+
+}
+
+function checkAllQuestionsAnswered(userSelections) {
+    return (userSelections.length == 5);
+}
+
+function getUserAnswers(userSelections) {
+    userAnswers = {};
+
+    userSelections.forEach((selection) => {
+        console.log(selection.name);
+        console.log(selection.id);
+    })
+}
